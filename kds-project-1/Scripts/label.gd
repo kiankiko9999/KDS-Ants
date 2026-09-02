a26390aed928dc89aeeabab86d1120aeea7939d7
LuckyMonkey809 <kevanmok@speedburst.com>
2026-09-01 22:57:33 -0700

    Skeleton Code for radial texture progress bar for the timer. Waiting on Thomas for a under texture and progress texture

--- kds-project-1/Scripts/label.gd
+++ kds-project-1/Scripts/label.gd
@@ -2,8 +2,10 @@ extends Label
 
 @onready var timer = $Timer
 @onready var antController = get_parent()
+@onready var timer_progress = $TextureProgressBar
+@export var initial_time: float = 120  # Set your countdown time here
 
-var time_left: float = 120  # Set your countdown time here
+var time_left = initial_time
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,14 +16,20 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	antController.points = antController.points
-	if time_left > 0:
-		time_left -= delta
-		time_left = max(time_left, 0.0)  # Prevents going negative
-		update_label()
-	elif time_left == 0:
+	# if time_left > 0:
+		# time_left -= delta
+		# time_left = max(time_left, 0.0)  # Prevents going negative
+		# update_label()
+	if time_left == 0:
 		get_parent().gameover()
 		pass
 		
 func update_label():
-	text = "Points: %d\nTime: %d\nNumber of ants: %d" % [antController.points, time_left, get_parent().ants_in_nest]
+	# text = "Points: %d\nTime: %d\nNumber of ants: %d" % [antController.points, time_left, get_parent().ants_in_nest]
+	text = "Points: %d\nNumber of ants: %d" % [antController.points, get_parent().ants_in_nest]
 	
+
+
+func _on_timer_timeout() -> void:
+	timer_progress.value -= 1
+	text = "Points: %d\nNumber of ants: %d" % [antController.points, get_parent().ants_in_nest]
