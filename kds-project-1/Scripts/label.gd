7ea474d2208232421d7b098d695baa86f438c9cd
kiankiko9999 <kiankiko9999@gmail.com>
2026-09-04 15:24:16 -0700

    Misc changes

--- kds-project-1/Scripts/label.gd
+++ kds-project-1/Scripts/label.gd
@@ -1,6 +1,6 @@
 extends Label
 
-@onready var timer = $Timer
+
 @onready var antController = get_parent()
 @onready var timer_progress = $TextureProgressBar
 @export var initial_time: float = 120  # Set your countdown time here
@@ -16,6 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	antController.points = antController.points
+<<<<<<< Updated upstream
 	if time_left > 0:
 		time_left -= delta
 		time_left = max(time_left, 0.0)  # Prevents going negative
@@ -28,6 +29,10 @@ func update_label():
 	# text = "Points: %d\nTime: %d\nNumber of ants: %d" % [antController.points, time_left, get_parent().ants_in_nest]
 	text = "Points: %d\n     : %d" % [antController.points, get_parent().ants_in_nest]
 	$TextureProgressBar.value = 100 * time_left/initial_time
+=======
+func update_label():
+	text = "Points: %d" % [antController.points]
+>>>>>>> Stashed changes
 	
 
 
